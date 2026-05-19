7d1623451d202a069a5dbf1688fefc57a42f5a10
Leo Nickl <leo@leonickl.de>
2026-05-19 15:01:01 +0200

    aggregate orders

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -31,4 +31,28 @@ class Order extends Model
     {
         return Item::all()->filter(fn (Item $item) => $item->order_id === $this->id);
     }
+
+    public function aggregate(): array
+    {
+        $groups = [];
+
+        foreach ($this->items() as $item) {
+            if (! array_key_exists($item->merch_id, $groups)) {
+                $groups[$item->merch_id] = (object)[
+                    'merch' => $item->merch(),
+                    'sizes' => [],
+                ];
+            }
+
+            $size = $item->size()?->label() ?? '---';
+
+            if (! array_key_exists($size, $groups[$item->merch_id]->sizes)) {
+                $groups[$item->merch_id]->sizes[$size] = 0;
+            }
+
+            $groups[$item->merch_id]->sizes[$size]++;
+        }
+
+        return $groups;
+    }
 }
